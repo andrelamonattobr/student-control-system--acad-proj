50db37d9d613ce671bc8f8492d3b002050d1734d
andrelamonattobr <andrelamonattobr@gmail.com>
2025-04-06 14:30:35 -0300

    Academic project, Student Attendance Control System or Student Control System

---
+++ src/main/java/com/scs/app/StudentControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.scs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentControlSystemApplication.class, args);
+	}
+
+}
